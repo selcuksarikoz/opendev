10a4dbebcdd8c47da646adaa8fbcf399ac9996f7
selcuksarikoz <benimpostahesabim@gmail.com>
2026-02-23 03:20:23 +0100

    release: 0.1.0

--- Formula/opendev.rb
+++ Formula/opendev.rb
@@ -1,20 +1,20 @@
 class Opendev < Formula
   desc "Terminal-first AI coding assistant focused on free/community models"
   homepage "https://github.com/selcuksarikoz/opendev"
-  version "0.1.0"
+  version "0.0.1"
 
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/selcuksarikoz/opendev/releases/download/v0.1.0/opendev-macos-arm64.tar.gz"
-      sha256 "REPLACE_WITH_SHA256"
+      url "https://github.com/selcuksarikoz/opendev/releases/download/v0.0.1/opendev-macos-arm64.tar.gz"
+      sha256 "8bf8441a60a3125490c890452ff0d75d245ba669ccee40a4bee050972e1c3710"
     elsif Hardware::CPU.intel?
-      url "https://github.com/selcuksarikoz/opendev/releases/download/v0.1.0/opendev-macos-x86_64.tar.gz"
-      sha256 "REPLACE_WITH_SHA256"
+      url "https://github.com/selcuksarikoz/opendev/releases/download/v0.0.1/opendev-macos-x86_64.tar.gz"
+      sha256 "8936134c2ad15fc4918141f7f0607b3daae005e91b8407093ee880db06bf314a"
     else
       odie "Unsupported macOS CPU."
     end
   else
-    odie "This formula currently ships macOS binaries only."
+    odie "Homebrew formula currently ships macOS binaries only."
   end
 
   def install
