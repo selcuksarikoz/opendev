a81f32b2a3931099590c0aa030d85bd121f349bd
selcuksarikoz <benimpostahesabim@gmail.com>
2026-02-23 03:29:38 +0100

    release: v0.1.1

--- Formula/opendev.rb
+++ Formula/opendev.rb
@@ -1,15 +1,15 @@
 class Opendev < Formula
   desc "Terminal-first AI coding assistant focused on free/community models"
   homepage "https://github.com/selcuksarikoz/opendev"
-  version "0.1.0"
+  version "0.1.1"
 
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/selcuksarikoz/opendev/releases/download/v0.1.0/opendev-macos-arm64.tar.gz"
-      sha256 "5cbab744f6a23dcf3c1fced8c1dda527f276106c36cce521c4ad07be4f866b14"
+      url "https://github.com/selcuksarikoz/opendev/releases/download/v0.1.1/opendev-macos-arm64.tar.gz"
+      sha256 "ca41201c928009dc76669242badf182f5eaa4ed43c30bf59b0756cf9f9edb024"
     elsif Hardware::CPU.intel?
-      url "https://github.com/selcuksarikoz/opendev/releases/download/v0.1.0/opendev-macos-x86_64.tar.gz"
-      sha256 "40a1522e7dc50bc41e062b7b504bdf5a360fdfbee6cf30276711839b1c6561a3"
+      url "https://github.com/selcuksarikoz/opendev/releases/download/v0.1.1/opendev-macos-x86_64.tar.gz"
+      sha256 "26a60eeb1e1184c7a5c18ab379856cea3426e8c3ff3cb80b8b50c5a75ef95c59"
     else
       odie "Unsupported macOS CPU."
     end
